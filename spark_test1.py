a575fee9108ab0c4cd808047b43785f0552adb53
aurorave <44628138+aurorave@users.noreply.github.com>
2023-01-05 14:29:50 +0900

    Add files via upload

---
+++ spark_test1.py
@@ -0,0 +1,41 @@
+# 아파치 스파크 학습을 위한 도커 환경 셋업하기 (feat.Zeppelin)
+# (https://kadensungbincho.tistory.com/91)
+
+# 1. docker-compose up
+# 2. Spark 실행해보기
+
+%pyspark
+
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, ArrayType
+# https://github.com/spark-examples/pyspark-examples/blob/master/pyspark-filter.py
+
+arrayStructureData = [
+    (("James","","Smith"),
+    ["Java","Scala","C++"], "OH", "M"),
+    (("Anna","Rose",""),
+    ["Spark","Java","C++"],"NY","F"),
+    (("Julia","","Williams"),
+    ["CSharp","VB"],"OH","F"),
+    (("Maria","Anne","Jones"),
+    ["CSharp","VB"],"NY","M"),
+    (("Jen","Mary","Brown"),
+    ["CSharp","VB"],"NY","M"),
+    (("Mike","Mary","Williams"),
+    ["Python","VB"],"OH","M")
+    ]
+    
+arrayStructureSchema = StructType([
+    StructField('name', StructType([
+        StructField('firstname', StringType(), True),
+        StructField('middlename', StringType(), True),
+        StructField('middlename', StringType(), True)
+        ])),
+        StructField('languages',
+        ArrayType(StringType()), True),
+        StructField('state', StringType(), True),
+        StructField('gender', StringType(), True)
+        ])
+
+df = spark.createDataFrame(data = arrayStructureData, schema = arrayStructureSchema)
+df.printSchema()
+df.show()
